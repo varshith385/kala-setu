277bc81859d9c83e9c4581f6b8bfd3e5c19339d9
Varshith <yourgithubemail@gmail.com>
2026-03-03 04:58:33 +0530

    Fixed correct cart context path

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCart } from "../../context/CartContext";
+import { useCart } from "../context/CartContext";
 import Link from "next/link";
 
 export default function CartPage() {
